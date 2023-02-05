620e89b22ee4425da37e9d61f35481dd274c18f8
knightch <amankumarpandey2397@gmail.com>
2023-02-06 00:11:49 +0530

    initial-setup for timeline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TimelineComponent } from './home/timeline/timeline.component';
+import { ProfileComponent } from './home/profile/profile.component';
+import { MessagesComponent } from './home/messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TimelineComponent,
+    ProfileComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
